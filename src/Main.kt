a016d7776a907007f2a87e8cc409cfd891efa3b9
ENZZO FERNANDES DAS CHAGAS <25203668@dsn127m1242314.cfp127aluno.local>
2026-08-17 16:57:42 -0300

    Tudo feito

---
+++ src/Main.kt
@@ -0,0 +1,56 @@
+fun main() {
+
+//    var idade: Int = 27
+//    var altura = 1.80
+//    val texto = "Bom dia, pessoal"
+//    println(texto)
+//    println(texto.length)
+//    println(texto.uppercase())
+//    println(texto.contains("pessoal"))
+//    println(idade)
+//    texto()
+
+
+//    val texto = """
+//        Texto escrito
+//        em várias linhas
+//        para teste
+//        """
+//
+//    println(texto)
+
+//    var caracter = 'e'
+//    println(caracter)
+
+
+//    val bloqueado: Boolean = false
+//    println(bloqueado)
+
+//    val idade: Int = 25
+//    println(idade)
+//    val populacao:Long = 7000000000000L
+//    println(populacao)
+
+//    val populacao: Long = 7_000_000_000L
+//    println(populacao)
+
+//    val peso: Float = 73.123456789F
+//    println(peso)
+
+    val altura: Double = 1.73
+    println(altura)
+
+
+    val x : Int = 10
+    val y: Double = x.toDouble()
+    val z: Double = x
+
+}
+
+fun texto(){
+    val nome: String = "joao"
+    val idade = 25
+
+    println("Bom dia $nome! Logo logo você fará ${idade + 1} anos!")
+
+}
