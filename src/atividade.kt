02a4922b1c22a74ecfe1cc5f7bf7c39b28219bfa
ENZZO FERNANDES DAS CHAGAS <25203668@dsn127m1242314.cfp127aluno.local>
2026-08-24 16:56:13 -0300

    Atividade feita

---
+++ src/atividade.kt
@@ -0,0 +1,97 @@
+fun main(){
+
+//    // EXERCICIO 1 -----------------------
+//    val numero1 = 4
+//    if(numero1 % 2 == 0){
+//        println("Numero Par")
+//
+//    }else{
+//        println("Numero Impar")
+//    }
+
+//    //EXERCICIO 2 -----------------------
+//    val numero2 = -5
+//    if(numero2 > 0){
+//        println("Numero positivo")
+//    }else if (numero2 == 0){
+//        println("Zero")
+//    }else{
+//        println("Numero negativo")
+//    }
+
+//    // EXERCICIO 3 --------------------------
+//    val idade = 15
+//    when(idade){
+//        in 0 until 12 -> println("Criança")
+//        in 12 .. 17 -> println("Adolescente")
+//        in 18 .. 59 -> println("Adulto")
+//        else -> println("Idoso")
+//    }
+
+
+//    // EXERCICIO 4 ---------------------------
+//    val numero3 = 3
+//    var cont: Int = 1
+//    while (cont <= 10){
+//        val result = numero3 * cont
+//        println("$numero3 x $cont = $result")
+//        cont++
+//    }
+
+//    // EXERCICIO 5 -----------------------------
+//    val numeros5 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//    val soma = numeros5.sum()
+//    println("A soma da lista de números é $soma")
+
+
+//    // EXERCICIO 6 ---------------------------------
+//    val numeros6 = listOf(3, 1, 8, 5, 4)
+//    val maior = numeros6.max()
+//    println("O maior numero do lista é o $maior")
+
+
+//    // EXERCICIO 7 ---------------------------
+//    val numero7 = 20
+//    var cont = 0
+//    while (cont <= numero7){
+//        val numero7 = numero7 - cont
+//        println(numero7)
+//        cont++
+//    }
+
+
+//    // EXERCICI0 8 -------------------------
+//    val numeros8 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//    var sumPares = 0
+//    for(i in numeros8){
+//        if( i % 2 == 0){
+//            sumPares += i
+//        }
+//    }
+//    println(sumPares)
+
+
+//    // EXERCICIO 9 -----------------------------
+//    val numero9 = 14
+//    val dobro = numero9 * 2
+//    println("O dobro do ${numero9} é igual a ${dobro}")
+
+
+
+//    // EXERCICIO 10 ------------------------------
+//    var nota1 = 13
+//    var nota2 = 8
+//    var nota3 = 5
+//    val media = (nota1 + nota2 + nota3) / 3
+//    println("A média das nota é ${media}")
+
+
+
+//    // EXERCICIO 11 ------------------------
+//    val celsius = 30
+//    val fahrenheit = (celsius * 1.8) + 32
+//    println("O valor em fahrenheit é ${fahrenheit}")
+
+
+}
+
